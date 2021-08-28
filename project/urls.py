b83d6630ae19ca114f9ace83f01a757077bb5e5e
Fahd Mannaa <egypingblog@gmail.com>
2021-08-28 21:56:10 +0300

    Category products paginated > Done

--- project/urls.py
+++ project/urls.py
@@ -9,21 +9,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # Graphene GUI tool
     path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
-
-    # Catalog urls 
-    # All Products 
-    # All Products per category, paginated 
-    # All Products per category, paginated, sorted by high price
-    # All Products per category, paginated, sorted by low price
-    # All Products per category, paginated, filtered to specific brand
-    # All Products per brand, paginated 
-    # All Products per brand, paginated, sorted by high price
-    # All Products per brand, paginated, sorted by low price
-    # All Products per brand, paginated, filtered to specific brand
-    # All Both languages 
-    # All categories > Done
-    # Category per slug > Done
-    # All Brands > Done
-    # Brand per slug > Done
-
 ]
